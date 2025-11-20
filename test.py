8870a940429c2823967316b0a16819a80d7420b0
Ray <lokto.kwan@gmail.com>
2025-11-20 10:05:01 -0500

    add state names

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
 import linuxcnc
 import time
 
+state_names = {
+    linuxcnc.STATE_ON: "STATE_ON",
+    linuxcnc.STATE_OFF: "STATE_OFF",
+    linuxcnc.STATE_ESTOP: "STATE_ESTOP",
+    linuxcnc.STATE_ESTOP_RESET: "STATE_ESTOP_RESET",
+}
+
+def print_state(task_state):
+    return state_names.get(task_state, "UNKNOWN_STATE")
+
 # Connect to LinuxCNC
 c = linuxcnc.command()
 s = linuxcnc.stat()
@@ -17,4 +27,4 @@ c.wait_complete()      # Wait for command to finish
 # Poll status and print positions
 s.poll()
 print("Axis positions:", s.actual_position)  # X, Y, Z
-print("Machine state:", s.task_state)
+print("Machine state:", print_state(s.task_state))
