638996133d109a0eae5b50c531e6053fe0a9616b
Ray Kwan <rkwan@skatescribe.com>
2025-11-20 16:46:01 -0500

    wrap in check before starting

--- run_mdi.py
+++ run_mdi.py
@@ -14,9 +14,14 @@ c.state(linuxcnc.STATE_ON)
 time.sleep(1)
 
 # Switch to MDI mode and send a command
-c.mode(linuxcnc.MODE_MDI)
-c.wait_complete()
-c.mdi("G0 X1 Y1 Z0.1")  # Move to coordinates
+def ok_for_mdi():
+    s.poll()
+    return not s.estop and s.enabled and (s.homed.count(1) == s.joints) and (s.interp_state == linuxcnc.INTERP_IDLE)
+
+if ok_for_mdi():
+    c.mode(linuxcnc.MODE_MDI)
+    c.wait_complete()
+    c.mdi("G0 X1 Y1 Z0.1")  # Move to coordinates
 c.wait_complete()  # Wait for command to finish
 
 # Poll status and print positions
